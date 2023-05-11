e39b47a6bfa69be85884dbd7f77f06ea6022274e
Dalton Donovan <90434265+daltonyxdonovan@users.noreply.github.com>
2023-05-11 12:37:56 -0500

    add gold command polish custom methods for command input

--- Plugin.cs
+++ Plugin.cs
@@ -4,16 +4,80 @@ using UnityEngine.UI;
 using TMPro;
 using System;
 using System.Text.RegularExpressions;
+using UnityEngine.SceneManagement;
 
 namespace ConsoleCommands
 {
     [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
     public class Plugin : BaseUnityPlugin
     {
+
+        ChatBox chatBox;
+        bool active = false;
+        int ticker = 0;
+        bool maxhp_active = false;
+        int maxhp_amount = 100;
         public void Log(string message)
         {
+            
             Logger.LogInfo(message);
-            ChatBox.Instance.SendMessage("<color=red>" + message + "</color>");
+            chatBox.SendMessage("<color=red>" + message + "</color>");
+        }
+
+        public void DropItemIntoWorld(InventoryItem item)
+        {
+            if (item == null)
+            {
+                return;
+            }
+            ClientSend.DropItem(item.id, item.amount);
+        }
+
+        public int AddItemToInventory(InventoryItem item)
+        {
+            InventoryItem inventoryItem = ScriptableObject.CreateInstance<InventoryItem>();
+            inventoryItem.Copy(item, item.amount);
+            InventoryCell inventoryCell = null;
+            UiSfx.Instance.PlayPickup();
+            if (AchievementManager.Instance)
+            {
+                AchievementManager.Instance.PickupItem(item);
+            }
+            InventoryUI inventoryUI = InventoryUI.Instance;
+            foreach (InventoryCell inventoryCell2 in inventoryUI.cells)
+            {
+                if (inventoryCell2.currentItem == null)
+                {
+                    if (!(inventoryCell != null))
+                    {
+                        inventoryCell = inventoryCell2;
+                    }
+                }
+                else if (inventoryCell2.currentItem.Compare(inventoryItem) && inventoryCell2.currentItem.stackable)
+                {
+                    if (inventoryCell2.currentItem.amount + inventoryItem.amount <= inventoryCell2.currentItem.max)
+                    {
+                        inventoryCell2.currentItem.amount += inventoryItem.amount;
+                        inventoryCell2.UpdateCell();
+                        UiEvents.Instance.AddPickup(inventoryItem);
+                        return 0;
+                    }
+                    int num = inventoryCell2.currentItem.max - inventoryCell2.currentItem.amount;
+                    inventoryCell2.currentItem.amount += num;
+                    inventoryItem.amount -= num;
+                    inventoryCell2.UpdateCell();
+                }
+            }
+            if (inventoryCell)
+            {
+                inventoryCell.currentItem = inventoryItem;
+                inventoryCell.UpdateCell();
+                MonoBehaviour.print("added to available cell");
+                UiEvents.Instance.AddPickup(inventoryItem);
+                return 0;
+            }
+            UiEvents.Instance.AddPickup(inventoryItem);
+            return inventoryItem.amount;
         }
 
         public string TrimMessage(string message)
@@ -27,13 +91,15 @@ namespace ConsoleCommands
 
         public void ClearMessage()
         {
-            ChatBox.Instance.inputField.text = "";
-		    ChatBox.Instance.inputField.interactable = false;
+            
+            chatBox.inputField.text = "";
+		    chatBox.inputField.interactable = false;
         }
 
         public void CustomSendMessage(string message)
         {
-            ChatBox.Instance.typing = false;
+            
+            chatBox.typing = false;
             message = TrimMessage(message);
             if (message == "")
             {
@@ -41,11 +107,17 @@ namespace ConsoleCommands
             }
             if (message[0] == '/')
             {
+               
+                // If the last character of the message is '/', remove it and return the updated message
+                if (message[message.Length - 1] == '/')
+                {
+                    message.Substring(0, message.Length - 1);
+                }
                 ChatCommand(message);
                 return;
             }
             
-            ChatBox.Instance.AppendMessage(0, message, GameManager.players[LocalClient.instance.myId].username);
+            chatBox.AppendMessage(0, message, GameManager.players[LocalClient.instance.myId].username);
             ClientSend.SendChatMessage(message);
             ClearMessage();
         }
@@ -54,158 +126,180 @@ namespace ConsoleCommands
         {
             // Plugin startup logic
             Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+            //find the object named chatBox
         }
 
         public void ChatCommand(string message)
         {
+            
             if (message.Length <= 0)
             {
                 return;
             }
-            string a = message.Split(new char[]
+            string command = message;
+            chatBox.inputField.text = "";
+		    chatBox.inputField.interactable = false;
+            
+
+
+            if (command.StartsWith("/heal"))
             {
-                ' '
-            })[0].Substring(1);
-            ChatBox.Instance.inputField.text = "";
-		    ChatBox.Instance.inputField.interactable = false;
-            string text = "#FFFFFF";
-            if (a == "seed")
-            {
-                int seed = GameManager.gameSettings.Seed;
-                ChatBox.Instance.AppendMessage(-1, string.Concat(new object[]
+                
+                int choice = int.Parse(command.Split()[1]);
+                
+                if (choice < 1)
                 {
-                    "<color=",
-                    text,
-                    ">Seed: ",
-                    seed,
-                    " (copied to clipboard)<color=white>"
-                }), "");
-                GUIUtility.systemCopyBuffer = string.Concat(seed);
-                return;
-            }
-            if (a == "ping")
-            {
-                ChatBox.Instance.AppendMessage(-1, "<color=" + text + ">pong<color=white>", "");
-                return;
-            }
-            if (a == "debug")
-            {
-                DebugNet.Instance.ToggleConsole();
-                return;
-            }
-            if (a == "kill")
-            {
-                PlayerStatus.Instance.Damage(0, 0, true);
-                return;
-            }
-
-            /*
-            ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-                            CUSTOM COMMANDS HERE    vvv
-            ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-            */
-
-            if (a.StartsWith("heal"))
-            {
-
-                int amount = 100;
-                if (a.Length > 4)
-                {
-                    if (!int.TryParse(a.Substring(5), out amount))
-                    {
-                        Log("used an invalid heal amount");
-                        return;
-                    }
-                }
-                else
-                {
-                    Log("healed with a default amount of 100");
-                }
-
-                PlayerStatus.Instance.Heal(amount);
-                Log($"healed {amount} hp");
-                return;
-            }
-
-            if (a.StartsWith("maxhp"))
-            {
-                PlayerStatus playerStatus = PlayerStatus.Instance;
-                int amount = 1000;
-
-                if (a.Length > 5)
-                {
-                    if (!int.TryParse(a.Substring(6), out amount))
-                    {
-                        Log("used an invalid maxHP amount");
-                        return;
-                    }
+                    choice = 1;
+                    Log("cannot use a value less than 1");
                 }
                 
+                
 
-                playerStatus.maxHp = amount;
-                playerStatus.hp = amount;
-                Log($"set their maxHP to {amount}");
-            }
-
-            if (!(a == "kick"))
-            {
+                PlayerStatus.Instance.Heal(choice);
+                Log($"healed {choice} hp");
                 return;
             }
-            int startIndex = message.IndexOf(" ", StringComparison.Ordinal) + 1;
-            string username = message.Substring(startIndex);
-            if (!GameManager.instance.KickPlayer(username))
+
+            if (command.StartsWith("/maxhp"))
             {
-                ChatBox.Instance.AppendMessage(0, "Failed to kick player...", GameManager.players[LocalClient.instance.myId].username);
+                maxhp_active = true;
+
+
+                PlayerStatus playerStatus = PlayerStatus.Instance;
+                int choice = int.Parse(command.Split()[1]);
+                
+                if (choice < 1)
+                {
+                    choice = 1;
+                }
+                maxhp_amount = choice;
+
+                playerStatus.maxHp = choice;
+                playerStatus.hp = choice;
+                Log($"set their maxHP to {choice}");
+            }
+
+            if (command.StartsWith("/gold"))
+            {
+                
+                InventoryCell[] cells = FindObjectsOfType<InventoryCell>();
+                bool done = false;
+                for (int i = 0; i < cells.Length; i++)
+                {
+                    if (!(cells[i].currentItem == null) && cells[i].currentItem.name == "Coin" && !done)
+                    {
+                        cells[i].currentItem.amount += 99999;
+                        done = true;
+                    }
+                }
+
+                if (!done)
+                {
+                    InventoryItem gold = ItemManager.Instance.GetItemByName("Coin");
+                    AddItemToInventory(gold);
+
+                }
+
+                Log($"added gold to inventory");
+                
             }
         }
 
-        public void EarlyUpdate()
-        {
-            if (Input.GetKeyDown(KeyCode.Return))
-            {
-                if (ChatBox.Instance.typing)
-                {
-                    CustomSendMessage(ChatBox.Instance.inputField.text);
-                }
-                else
-                {
-                    if (ChatBox.Instance.typing)
-                    {
-                        return;
-                    }
-                    ChatBox.Instance.typing = false;
-                    ChatBox.Instance.overlay.CrossFadeAlpha(0f, 1f, true);
-                    ChatBox.Instance.messages.CrossFadeAlpha(0f, 1f, true);
-                    ChatBox.Instance.inputField.GetComponent<Image>().CrossFadeAlpha(0f, 1f, true);
-                    ChatBox.Instance.inputField.GetComponentInChildren<TextMeshProUGUI>().CrossFadeAlpha(0f, 1f, true);
-                    ChatBox.Instance.inputField.interactable = true;
-                    ChatBox.Instance.inputField.Select();
-                    ChatBox.Instance.typing = true;
-                }
-            }
-            if (ChatBox.Instance.typing && !ChatBox.Instance.inputField.isFocused)
-            {
-                ChatBox.Instance.inputField.Select();
-            }
-            if (Input.GetKeyDown(KeyCode.Escape) && ChatBox.Instance.typing)
-            {
-                ChatBox.Instance.inputField.text = "";
-		        ChatBox.Instance.inputField.interactable = false;
-                ChatBox.Instance.typing = false;
-                base.CancelInvoke("HideChat");
-                base.Invoke("HideChat", 5f);
-            }
-        }
 
         public void Update()
         {
-            //if f1 is pressed, Log("F1 is pressed") will be called
-            if (Input.GetKeyDown(KeyCode.F1))
+            if (SceneManager.GetActiveScene().name != "GameAfterLobby") return;
+            
+            //if active and scene name is 'GameAfterLobby'
+            if (!active && ticker == 0)
             {
-                Log("F1 is pressed");
+                //try to reference chat without causing an error
+                chatBox = GameObject.Find("Chat").GetComponent<ChatBox>();
+                if (chatBox == null)
+                    active = false;
+                if (chatBox != null)
+                    active = true;
             }
-            
-            
+
+            if (!active)
+            {
+                ticker++;
+                if (ticker > 120)
+                {
+                    
+                    ticker = 0;
+                }
+                   
+                
+            }
+            else if (active)
+            {
+                ticker++;
+                if (ticker > 600)
+                {
+                    if (maxhp_active)
+                    {
+                        PlayerStatus playerStatus = PlayerStatus.Instance;
+                        playerStatus.maxHp = maxhp_amount;
+                        playerStatus.hp = maxhp_amount;
+                    }
+                    
+                }
+                if (Input.GetKeyDown(KeyCode.Slash))
+                {
+
+                    
+                    if (chatBox.typing)
+                    {
+                        // If the last character of the message is '/', remove it and return the updated message
+                        if (chatBox.inputField.text[chatBox.inputField.text.Length - 1] == '/')
+                        {
+                            //trim the last character
+                            chatBox.inputField.text = chatBox.inputField.text.Substring(0, chatBox.inputField.text.Length - 1);
+
+                        }
+                        CustomSendMessage(chatBox.inputField.text);
+                    }
+                    else
+                    {
+                        if (chatBox.typing)
+                        {
+                            return;
+                        }
+                        chatBox.typing = false;
+                        chatBox.overlay.CrossFadeAlpha(0f, 1f, true);
+                        chatBox.messages.CrossFadeAlpha(0f, 1f, true);
+                        chatBox.inputField.GetComponent<Image>().CrossFadeAlpha(0f, 1f, true);
+                        chatBox.inputField.GetComponentInChildren<TextMeshProUGUI>().CrossFadeAlpha(0f, 1f, true);
+                        chatBox.inputField.interactable = true;
+                        chatBox.inputField.Select();
+                        chatBox.typing = true;
+                    }
+                }
+                if (chatBox.typing && !chatBox.inputField.isFocused)
+                {
+                    chatBox.inputField.Select();
+                    //add a '/' to text box
+                    chatBox.inputField.MoveTextEnd(false);
+                    chatBox.inputField.text = "/";
+                    //move caret to teh right
+                    chatBox.inputField.caretPosition = 1;
+
+                }
+                if (Input.GetKeyDown(KeyCode.Escape) && chatBox.typing)
+                {
+                    chatBox.inputField.text = "";
+                    chatBox.inputField.interactable = false;
+                    chatBox.typing = false;
+                    base.CancelInvoke("HideChat");
+                    base.Invoke("HideChat", 5f);
+                }
+
+
+            }
+
+
+
         }
     }
 }
